ddbb1f707f25c0c68f74311ffedb95420861f5f7
Alex Winkler <FO-nTTaX@users.noreply.github.com>
2023-09-25 19:54:36 +0200

    sanity check string length before accessing string value by index

--- includes/PF_FormPrinter.php
+++ includes/PF_FormPrinter.php
@@ -992,7 +992,7 @@ END;
 				$brackets_end_loc = strpos( $section, "}}}", $brackets_loc );
 				// For cases with more than 3 ending brackets,
 				// take the last 3 ones as the tag end.
-				while ( $section[$brackets_end_loc + 3] == "}" ) {
+				while ( $brackets_end_loc + 3 < strlen( $section ) && $section[$brackets_end_loc + 3] == "}" ) {
 					$brackets_end_loc++;
 				}
 				$bracketed_string = substr( $section, $brackets_loc + 3, $brackets_end_loc - ( $brackets_loc + 3 ) );
